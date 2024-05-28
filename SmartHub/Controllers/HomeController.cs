1bfbe5b06c6a80752b9313ab9bfe1e1034812ba3
Po-Lung Wu <wupojung@gmail.com>
2024-05-29 01:44:42 +0800

    追加Top N 功能

--- SmartHub/Controllers/HomeController.cs
+++ SmartHub/Controllers/HomeController.cs
@@ -20,4 +20,11 @@ public class HomeController : Controller
         var data = await _pzemService.ListAsync(10);
         return await Task.Run(() => View(data));
     }
+
+    [HttpGet("Top/{count?}")]
+    public async Task<IActionResult> Top(int count = 10)
+    {
+        var data = await _pzemService.ListAsync(count);
+        return await Task.Run(() => View("Index",data));
+    }
 }
